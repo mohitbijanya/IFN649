712b1a3b01fc125564f400e8f9b78301cda2d4b6
Chris <chandrachristopher43@gmail.com>
2021-09-14 17:03:02 +1000

    Our Shell Script

---
+++ Week 8/client.py
@@ -0,0 +1,22 @@
+import paho.mqtt.client as mqtt
+
+
+def on_connect(client, userdata, flags, rc): # func for making connection
+ print("Connected to MQTT")
+ print("Connection returned result: " + str(rc) )
+ client.subscribe("soilmoisture")
+ client.subscribe("photoresistor")
+ client.subscribe("humidity")
+ client.subscribe("temperature")
+ client.subscribe("heatindex")
+ 
+
+def on_message(client, userdata, msg): # Func for Sending msg
+ print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+print(on_message.msg.topic + "success")
+client.connect("172.31.20.137", 1883, 60)
+client.loop_forever()
